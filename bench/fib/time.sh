88f08d2b9dd2120b95e56afe2e8bff6ba955f7bb
Teddy Randby <tedrandby@gmail.com>
2024-02-14 15:08:56 +0000

    Tailcalls working

--- bench/fib/time.sh
+++ bench/fib/time.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/bash
 
-hyperfine -w 8 -r 10 'python3 fib.py' 'gab fib.gab' 'lua fib.lua' 'ruby fib.rb' 'gab fib_block.gab'
+hyperfine -w 8 -r 30 'python3 fib.py' 'gab fib.gab' 'lua fib.lua' 'ruby fib.rb' 'gab fib_block.gab'
