8d86409e5a904c67d0ac54ab2cd562e0e71a6570
Teddy Randby <tedrandby@gmail.com>
2024-03-20 03:14:34 +0000

    Remove bad bench code

--- bench/mat/time.sh
+++
@@ -1,3 +0,0 @@
-#!/usr/bin/bash
-
-hyperfine -w 8 -r 10 'python3 mat.py' 'gab mat.gab' 'ruby mat.rb'
--- bench/prop/time.sh
+++
@@ -1,3 +0,0 @@
-#!/usr/bin/bash
-
-hyperfine -w 5 -r 10 "gab ./prop.gab" "python ./prop.py" "ruby ./prop.rb" "lua prop.lua"
