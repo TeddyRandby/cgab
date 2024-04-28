0faa1375d9ebbb9654304baaaae4aa08a4ea0177
Teddy Randby <tedrandby@gmail.com>
2024-04-28 16:28:03 +0000

    Cool stuff

---
+++ .clide/{file}/PerformanceCheck.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+
+cd "$CLIDE_PATH/../" || exit 1
+
+valgrind --tool=callgrind --callgrind-out-file=callgrind.out --dump-line=yes gab "$file"
+
+~/pyenv/bin/gprof2dot --format=callgrind callgrind.out > dot.out
+
+dot -Tpng dot.out -o graph.out.png
+
+feh graph.out.png
+
+rm *.out*
